c0e074d2a45bfb57a7e5cde19bb5e23f56663500
morining <mvclogic@gmail.com>
2018-12-23 14:40:53 +0800

    add broswerlist in gulpfile

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -3,7 +3,7 @@ var babel = require('gulp-babel')
 var print = require('gulp-print').default
 var clean = require('gulp-clean')
 var uglify = require('gulp-uglify')
-var  plumber = require('gulp-plumber');
+var plumber = require('gulp-plumber');
 
 gulp.task('clean-scripts', function () {
     return gulp.src('wwwroot/js/dist/**/*.js', {
@@ -12,20 +12,27 @@ gulp.task('clean-scripts', function () {
         .pipe(clean());
 });
 
-gulp.task("js-build",function(){
-  return gulp
-      .src('wwwroot/js/src/**/*.js') 
-      .pipe(print())
-      .pipe(plumber())
-      .pipe(babel({
-          presets: ['@babel/env'],
-          plugins:[
+gulp.task("js-build", function () {
+    return gulp
+        .src('wwwroot/js/src/**/*.js')
+        .pipe(print())
+        .pipe(plumber())
+        .pipe(babel({
+            presets: [
+                [
+                    "@babel/env",
+                    {
+                        targets: {
+
+                            browsers: ["since 2015", "ie >= 9"]
+                        }
+                    }
+                ]
+            ],
+            plugins: [
                 '@babel/plugin-transform-modules-umd',
-          ]
-      }))
-      //.pipe(uglify())
-      .pipe(gulp.dest('wwwroot/js/dist')) // #4. copy the results to the build folder
+            ]
+        }))
+        //.pipe(uglify())
+        .pipe(gulp.dest('wwwroot/js/dist')) // #4. copy the results to the build folder
 })
-
-
-
