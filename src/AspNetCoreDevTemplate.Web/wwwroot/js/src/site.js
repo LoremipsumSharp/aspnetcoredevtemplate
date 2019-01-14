0de2cf91714fd0b00fce8ee42dc47f0be06e55a3
loremipsum <289989941@qq.com>
2019-01-15 03:00:36 +0800

    add jquery plugin devlopment pattern

--- src/AspNetCoreDevTemplate.Web/wwwroot/js/src/site.js
+++ src/AspNetCoreDevTemplate.Web/wwwroot/js/src/site.js
@@ -1,6 +1,8 @@
 ﻿import $ from 'jquery'
-import 'jquery-validation'
+import '../../lib/jquery-validation/dist/jquery.validate'
 import { layer } from "./header";
 
-// layer.alert(`jquery.validate loaded?${$().validate instanceof Function}`)
-layer.msg("Hello World")
+
+
+layer.alert(`jquery.validate loaded?${$().validate instanceof Function}`) 
+//layer.msg("Hello World")
